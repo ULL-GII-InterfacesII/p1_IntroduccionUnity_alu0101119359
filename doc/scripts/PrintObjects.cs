c1fc1ee14ee2efd9d36174c92af67feba3d2459f
BuildTools <unconfigured@null.spigotmc.org>
2020-10-24 10:20:07 +0000

    added documentation

---
+++ doc/scripts/PrintObjects.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintObjects : MonoBehaviour
+{
+
+	private Hashtable objects = new Hashtable();
+	private Hashtable counter = new Hashtable();
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		GameObject[] rootObjects = gameObject.scene.GetRootGameObjects() ;
+
+		int key = 0;
+		foreach (GameObject child in rootObjects)
+		{
+			objects.Add(key, child);
+			counter.Add(key, 0);
+			key++;
+		}
+
+		foreach (DictionaryEntry de in objects)
+		{
+			Debug.Log( de.Key.ToString() +"-"+ de.Value.ToString() +" "+ counter[de.Key].ToString());
+		}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
